5fcc767f1c4df3634b609581093b0616b4c9d378
Dev. Bruno Oliveira <bholiveira.web@hotmail.com>
2021-10-06 16:39:10 -0300

    update package

--- resources/views/layouts/includes/development-feature.blade.php
+++ resources/views/layouts/includes/development-feature.blade.php
@@ -1,4 +1,4 @@
 <div class="alert alert-danger">
-    <h3>Recuso em desenvolvimento!</h3>
+    <h3>Recurso em desenvolvimento!</h3>
     É possível haver <strong>instabilidades</strong>, <strong>indisponibilidade</strong> ou que as <strong>informações estejam fixadas</strong>.
 </div>
