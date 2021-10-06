5f90ebee73754618048898b1163a37cb616bf3e4
Dev. Bruno Oliveira <bholiveira.web@hotmail.com>
2021-10-06 12:48:04 -0300

    deploy heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Schema::defaultStringLength(191);
     }
 }
